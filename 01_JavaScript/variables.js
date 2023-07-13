094080eff82de6dcd953413afc941c3371e08557
Mallikarjun_MK <mallikarjun.mkstar@gmail.com>
2023-07-13 19:11:35 +0000

    terminal font size changed

--- 01_JavaScript/variables.js
+++ 01_JavaScript/variables.js
@@ -1,7 +1,24 @@
 const accountId = 12223423;
 let costemer = "Mallikarjun";
 var emailId = "abd@gmail.com";
+var password = "xyz@123"
 cityName = "Bengaluru";
 let stateName;
 
-console.log(accountId);
+// -> single line comment , makes code comment from where it is put to the end of the line
+/*
+...
+multiline comment
+...
+*/
+/*
+   comments are use to leave notes and to test how block code works
+ */
+//accountId = 2232342;//It's not valid beacuse constant variable cannot be changed or reassigned once it is assigned
+emailId = "xyz@gmail.com";
+password = "pqe@321";
+console.log("Customer Details");
+console.table([accountId,costemer,emailId,password,cityName,stateName]);
+
+//Note
+// Prefer not to use var because it has function scope but not block scope and use let because it has block scope
