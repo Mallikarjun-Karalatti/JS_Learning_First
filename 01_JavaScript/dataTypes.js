a8a115e584dd7013f9bb1b6b8d1bb92a1028be28
Mallikarjun_MK <mallikarjun.mkstar@gmail.com>
2023-07-14 17:16:31 +0000

    datatypes summary added

--- 01_JavaScript/dataTypes.js
+++ 01_JavaScript/dataTypes.js
@@ -2,8 +2,9 @@
 // alert(3+3); -> we are using nodejs not browser
 
 //DataTypes in JS
+// two types premitive and non-premitive(reference type) based on how data is stored in memory and access from memory
 /*
-    a)Premitive
+    a)Premitive => call by value (copy of data is given when fetched and reference of address in memory is not given)
         TYPES     :   values 
       1)number    => 
       2)string    => from empty string("") with zero 
@@ -15,9 +16,13 @@
                      set of all non-String values that may be used as the key of an Object property 
       7)bigint    =>
 
-     b)UserDefined
-       8)Object 
+     b)Non-premitive or Reference
+       8)Object
+       9)Arrays
+       10)Functions 
  */
+     
+       //JS is dynamically typed lang because we don't need to specify the datatype specifically while declaring variables
 
        //examples
        let name = "Mallikarjun"; //string
@@ -25,6 +30,20 @@
        let isDeveloper = true;   //boolean
        let  inComplete;        //undefined
        let whtsInThere = null;   //null
+       let id = Symbol('123') //symbol
+       let anotherId = Symbole('123') //symbol
+       const bigNumber = 23525421451451453235n;
+
+       const heroes = ["Captain America", "Superman", "Batman","Spiderman"]; //Array 
+       const myObj = {
+          iAm: "Mallikarjun",
+          age: 21
+       }   // object
+       const myFunction = function(){
+        console.log("Hello world");
+       }
+
 
        console.log(typeof inComplete); // => type undefined
-       console.log(typeof(whtsInThere)); // => type object **Important**
+       console.log(typeof(whtsInThere)); // => type object **Important** typeof gives object
+       console.log(id === anotherId) ; // false as symbol will be uique
