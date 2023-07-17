b56186807d2c7a95cd3901c2d5ddf8cc14719f0c
Mallikarjun_MK <mallikarjun.mkstar@gmail.com>
2023-07-17 17:02:33 +0000

    different methods of string are discussed

--- 01_JavaScript/string.js
+++ 01_JavaScript/string.js
@@ -5,7 +5,7 @@ let repoCount = 22;
 console.log(`Hi!, my name is ${myName} and I have ${repoCount} repositories in my github profile.`);
 
 //another way of declaring string
-let gameName = new String('Pokemon'); //it is an object with key value pairs where key will be index and value will be charecter at that index
+let gameName = new String('Pokemon-Game'); //it is an object with key value pairs where key will be index and value will be charecter at that index
 console.log(gameName[0]); // gives 'P'
 console.log(gameName.__proto__); //prototype, o/p will be {} and key value pairs in it
 console.log(gameName.length); // legth is property of this string object and accessed using dot operator
@@ -14,3 +14,18 @@ console.log(gameName.charAt(2));
 console.log(gameName.indexOf('o'));
 console.log(gameName.indexOf('x'));
 
+
+const newString = myName.substring(6,11); //considers only positive values as argument
+console.log(newString);
+
+const anotherString = myName.slice(2,5); //can take negative index
+console.log(anotherString);
+
+const newStringTwo = "  Arjun  ";
+console.log(newStringTwo);
+console.log(newStringTwo.trim()); // startTrim() & endTrim() also there
+
+const url = "https://abcGoggle.com/hello%20world";
+console.log(url.replace('%20','-'));
+console.log(url.includes('xyz'));
+console.log(gameName.split('-'));
