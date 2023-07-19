578e88e46011a28ef1fa656688f59b133d2b0811
Mallikarjun_MK <mallikarjun.mkstar@gmail.com>
2023-07-19 12:58:38 +0000

    difference between premitive and object string

--- 01_JavaScript/string.js
+++ 01_JavaScript/string.js
@@ -1,15 +1,29 @@
-let myName = "Mallikarjun"; //string declaration
+let myName = "Mallikarjun"; //string declared as premitive
 let repoCount = 22;
 
-//string interpolation
-console.log(`Hi!, my name is ${myName} and I have ${repoCount} repositories in my github profile.`);
+// template literal used to string interpolation
+console.log(`Hi!, my name is ${myName} and I have ${repoCount} repositories in my GitHub profile.`);
 
-//another way of declaring string
-let gameName = new String('Pokemon-Game'); //it is an object with key value pairs where key will be index and value will be charecter at that index
-console.log(gameName[0]); // gives 'P'
-console.log(gameName.__proto__); //prototype, o/p will be {} and key value pairs in it
-console.log(gameName.length); // legth is property of this string object and accessed using dot operator
-console.log(gameName.toUpperCase()); //calling different funtions/methods on string and here object value will not be changed beacuse strings are immutable in JS.
+//another way of declaring a string as an object (type object)
+
+let gameName = new String('Pokemon-Game'); 
+//it is an object with key-value pairs where the key will be indexed and the value will be a character at that index
+
+//Difference between primitive and object string
+let str1 = "2+2";
+let str2 = new String("2+2");
+console.log(eval(str1)); //gives 4
+console.log(eval(str2)); //gives  [String: '2+2']
+
+//object -> premitive
+console.log(eval(str2.valueOf()));
+
+console.log(gameName[0]); 
+// gives 'P' here-string is treated as an array of characters and in this way you can only fetch characters and can not delete or modify them
+
+console.log(gameName.__proto__); //prototype, o/p will be {} and key-value pairs in it
+console.log(gameName.length); // length is property of this string object and accessed using dot operator
+console.log(gameName.toUpperCase()); //calling different functions/methods on a string and here object value will not be changed because strings are immutable in JS.
 console.log(gameName.charAt(2));
 console.log(gameName.indexOf('o'));
 console.log(gameName.indexOf('x'));
