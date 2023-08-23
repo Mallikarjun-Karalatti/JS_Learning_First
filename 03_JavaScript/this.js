6401c803f62ed9ae6076a46ce6a400e55fba8f47
Mallikarjun_MK <mallikarjun.mkstar@gmail.com>
2023-08-23 06:05:29 +0000

    this and arrow funcitons discussed

---
+++ 03_JavaScript/this.js
@@ -0,0 +1,41 @@
+const user = {
+    username: "Chrish",
+    price: 999,
+
+    welcomeMessage: function(){
+        console.log(`${this.username}, welcome to website`);
+        console.log(this);
+    }
+}
+
+user.welcomeMessage();
+//user.username = "Sam";
+user.welcomeMessage();
+console.log(this);
+
+/*
+function chai(){
+    let username = "Evans";
+    console.log(this);
+    console.log(this.username);
+}
+chai();
+*/
+
+// Arrow functions
+
+const chai = () => {
+    let username = "Peter";
+    console.log(this);
+}
+chai();
+//explicit return --> using return keyword
+// const addTwo = (num1,num2) => {
+//     return num1+num2;
+// }
+
+//implicit return 
+//const addTwo =addTwo = (num1,num2) => num1+num2;
+//const addTwo =addTwo = (num1,num2) => (num1+num2);
+//const addTwo =addTwo = (num1,num2) => ({name: "Robin"});
+console.log(addTwo(3,4));
